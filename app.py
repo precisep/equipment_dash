2a11f416a18bc25af754bf7ab9847ccb7fa7a682
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2024-10-31 18:06:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import os
-import sqlite3
 import pandas as pd
 import dash
-from dash import Dash, dcc, html, Input, Output, State
+from dash import Dash, dcc, html, Input, Output
 from concurrent.futures import ThreadPoolExecutor  
 import plotly.graph_objects as go
-from datetime import datetime, timedelta
-from dotenv import load_dotenv
 import requests
 import plotly.io as pio
+from dotenv import load_dotenv
 
 # Set the default Plotly template
 pio.templates.default = "plotly_dark"
@@ -36,18 +34,11 @@ app.layout = html.Div(
     children=[
         html.H1("Aluecor Alarms Dashboard", className='app-heading'),
 
-        dcc.DatePickerSingle(
-            id='date-picker',
-            date=(datetime.now() - timedelta(1)).date(),
-            display_format='YYYY-MM-DD',
-            style={'margin': '20px'}
-        ),
-
         dcc.Loading(
             id="loading-spinner-generate",
             type="circle",
             children=[
-                html.Button('Generate Figure', id='generate-figure-btn', n_clicks=0),
+                html.Button('Fetch Data', id='fetch-data-btn', n_clicks=0),
                 html.Div(id='loading-status')
             ],
             fullscreen=False
@@ -58,21 +49,18 @@ app.layout = html.Div(
             type='circle',
             children=[
                 html.Div(
-                    id='output-graph',
-                    style={'display': 'flex', 'flex-wrap': 'wrap', 'justify-content': 'space-around'}
+                    id='output-data',
+                    style={'display': 'flex', 'flex-direction': 'column'}
                 ),
             ]
         ),
     ]
 )
 
-# Function to fetch data from API with pagination
-def fetch_page(page, start_date, end_date, page_size):
+# Function to fetch data from API without date filtering
+def fetch_data():
     try:
-        filter_query = (f'?fields=["tslast","tsactive","alarm","time_difference_minutes"]'
-                        f'&filters=[["tsactive",">=","{start_date}"],'
-                        f'["tsactive","<=","{end_date}"]]'
-                        f'&limit={page_size}&offset={page * page_size}')
+        filter_query = '?fields=["tslast","tsactive","alarm","time_difference_minutes"]&limit=200000'
         
         response = requests.get(f'{api_url}{filter_query}', headers=headers)
         response.raise_for_status()
@@ -80,164 +68,37 @@ def fetch_page(page, start_date, end_date, page_size):
         return response.json().get('data', [])
     
     except requests.exceptions.RequestException as e:
-        print(f"Error fetching page {page}: {str(e)}")
+        print(f"Error fetching data: {str(e)}")
         return []
 
-# Function to parse data from the API for a selected date
-def parse_frappe_api(selected_date):
-    start_date = f"{selected_date} 04:00:00"
-    end_date = f"{selected_date} 17:00:00"
-    
-    data = []  
-    page_size = 200000
-    initial_data = fetch_page(0, start_date, end_date, page_size)
-    data.extend(initial_data)
-
-    with ThreadPoolExecutor() as executor:
-        results = list(executor.map(lambda p: fetch_page(p, start_date, end_date, page_size), range(1, 10)))  # Adjust range as needed
-
-    for page_data in results:
-        data.extend(page_data)
-
+# Function to parse data from the API
+def parse_frappe_api():
+    data = fetch_data()
     df = pd.DataFrame(data)
-    print(f'Records for selected date: {df.shape[0]}')  # Print number of records found
+    print(f'Records fetched: {df.shape[0]}')  # Print number of records found
     if df.empty:
-        return "No data found for the selected date range."
+        return "No data found."
 
     return df.drop_duplicates()
 
-def create_figure(selected_date, df):
-    start_date = f"{selected_date} 07:00:00"
-    end_date = f"{selected_date} 17:00:00"
-
-  
-    start_date_filter = pd.to_datetime(start_date)
-    end_date_filter = pd.to_datetime(end_date)
-    mask = (df['tsactive'] >= start_date_filter) & (df['tsactive'] < end_date_filter)
-    filtered_df = df[mask]
-
-    filtered_df['Equipment Group'] = filtered_df['alarm'].apply(lambda alarm: map_to_equipment_group(alarm, equipment_grouping)).dropna()
-
-   
-    alarm_downtime_totals = filtered_df.groupby(['Equipment Group'])['time_difference_minutes'].sum().reset_index()
-    alarm_downtime_totals = alarm_downtime_totals.sort_values('time_difference_minutes', ascending=False)
-
-    filtered_df['Equipment Group'] = pd.Categorical(filtered_df['Equipment Group'], categories=alarm_downtime_totals['Equipment Group'], ordered=True)
-    filtered_df = filtered_df.sort_values('Equipment Group', ascending=False)
-    filtered_df['tslast'] = pd.to_datetime(filtered_df['tslast'])
-    time_range = pd.date_range(start=start_date, end=end_date, freq='H')
-
-    fig = go.Figure()
-
-    bar_width = 0.25
-
-  
-    for equipment_group in filtered_df['Equipment Group'].unique():
-        alarm_data = filtered_df[filtered_df['Equipment Group'] == equipment_group]
-        last_time = pd.Timestamp(start_date)
-
-        for hour in time_range:
-            active_alarms = alarm_data[(alarm_data['tslast'] >= last_time) & (alarm_data['tslast'] < hour)]
-            downtime_total = active_alarms['time_difference_minutes'].sum()
-            alarms_list = active_alarms['alarm'].unique()
-
-            if downtime_total > 0:
-                if (hour - last_time).total_seconds() / 60 > 0:
-                    active_time = (hour - last_time).total_seconds() / 60 - downtime_total
-                    start_time = active_alarms['tsactive'].iloc[0] if not active_alarms.empty else last_time
-                    
-                    start_time_in_minutes = (start_time - pd.Timestamp(start_time.date())).total_seconds() / 60
-                    
-                    alarms_hover = "<br>".join(alarms_list)
-
-                    fig.add_trace(go.Bar(
-                        y=[equipment_group],
-                        x=[active_time],
-                        width=bar_width,
-                        orientation='h',
-                        name='Good State',
-                        marker_color='lightgreen',
-                        base=last_time.hour * 60 + last_time.minute,
-                        hovertemplate=f'Start Time: {start_time.strftime("%Y-%m-%d %H:%M")} <br>Equipment: {equipment_group} Type: Good State<br>Duration: {minutes_to_hhmm(round(active_time, 2))}<extra></extra>',
-                        showlegend=False
-                    ))
-
-                    alarm_start_time_in_minutes = last_time.hour * 60 + last_time.minute + active_time
-                    alarm_start_time = last_time + pd.to_timedelta(active_time, unit='m')  
-                 
-                    fig.add_trace(go.Bar(
-                        y=[equipment_group],
-                        x=[downtime_total],
-                        width=bar_width,
-                        orientation='h',
-                        name='Active Alarm',
-                        marker_color='red',
-                        base=last_time.hour * 60 + last_time.minute + active_time,
-                        hovertemplate=f'Start Time: {alarm_start_time.strftime("%Y-%m-%d %H:%M")} <br>Equipment: {equipment_group} <br>Alarms: {alarms_hover}<br>Type: Active Alarm<br>Duration: {minutes_to_hhmm(round(downtime_total, 2))}<extra></extra>',
-                        showlegend=False
-                    ))
-                last_time = hour
-
-        # Add remaining time if applicable
-        if last_time < time_range[-1]:
-            remaining_time = (time_range[-1] - last_time).total_seconds() / 60
-            fig.add_trace(go.Bar(
-                y=[equipment_group],
-                x=[remaining_time],
-                width=bar_width,
-                orientation='h',
-                name='Good State',
-                marker_color='lightgreen',
-                base=last_time.hour * 60 + last_time.minute,
-                hovertemplate=f'Equipment: {equipment_group} <br>Alarms: None<br>Type: Good State<br>Duration: {minutes_to_hhmm(round(remaining_time, 2))}<extra></extra>',
-                showlegend=False
-            ))
-
-    # Update figure layout
-    fig.update_layout(
-        title="Active Alarm and Good State Duration by Alarm and Equipment Group",
-        xaxis_title="Timestamp (Minutes)",
-        yaxis_title="Equipment Group and Alarm",
-        barmode='stack',
-        height=800,
-        xaxis=dict(
-            tickmode='array',
-            tickvals=[(hour.hour * 60) for hour in time_range],
-            ticktext=[hour.strftime('%Y-%m-%d %H:%M') for hour in time_range],
-            showgrid=True
-        ),
-        yaxis=dict(tickfont=dict(size=10)),
-        font=dict(color='white')
-    )
-
-    return fig
-
 @app.callback(
-    Output('output-graph', 'children'),
-    Input('generate-figure-btn', 'n_clicks'),
-    State('date-picker', 'date')
+    Output('output-data', 'children'),
+    Input('fetch-data-btn', 'n_clicks')
 )
-def update_graph(n_clicks, selected_date):
+def update_data(n_clicks):
     if n_clicks > 0:
-        df_alarms = parse_frappe_api(selected_date)
+        df_alarms = parse_frappe_api()
         if isinstance(df_alarms, str): 
             return html.Div([html.P(df_alarms)])
 
-        bar_fig = create_figure(selected_date, df_alarms)
+        # Display the first few rows of the DataFrame
         return [
-            dcc.Graph(
-                id='bar-graph',
-                figure=bar_fig,
-                config={'displayModeBar': True}
-            )
+            html.Div([
+                html.P("Data fetched from API:"),
+                dcc.Markdown(df_alarms.head().to_markdown())  # Display DataFrame head in Markdown
+            ])
         ]
     return []
-
-
-def minutes_to_hhmm(minutes):
-    hours = int(minutes // 60)
-    minutes = int(minutes % 60)
-    return f"{hours}h {minutes}m"
 
 
 if __name__ == '__main__':
